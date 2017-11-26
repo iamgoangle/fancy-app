76a10ae4d7b6e56721fc674e3de1f858ff9f4c65
Teerapong Singthong <teerapong.singthong@krungthai-axa.co.th>
2017-11-26 21:38:07 +0700

    Fixed shellscript to add mongo collection

--- db/import.sh
+++ db/import.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "currency collection has been imported"
-mongoimport --db dbName --collection collectionName --file fileName.json --jsonArray
+mongoimport --db fancy-app --collection currencies --file ./collections/currencies.json --jsonArray
 
 echo "timezone collection has been imported"
-mongoimport --db dbName --collection collectionName --file fileName.json --jsonArray
+mongoimport --db fancy-app --collection timezones --file ./collections/timezones.json --jsonArray
 
-echo "language collection has been imported"
-mongoimport --db dbName --collection collectionName --file fileName.json --jsonArray
+echo "languages collection has been imported"
+mongoimport --db fancy-app --collection languages --file ./collections/languages.json --jsonArray
 
-echo "use collection has been imported"
-mongoimport --db dbName --collection collectionName --file fileName.json --jsonArray
+echo "users collection has been imported"
+mongoimport --db fancy-app --collection users --file ./collections/users.json --jsonArray
